84ef3cca342c69783491666ce00c79d61b559b56
SHRAVAN D A <shravan8704@gmail.com>
2023-09-29 22:19:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/QuizAppReact/',
+  build:{
+    rollupOptions:{
+      output:{
+        publicPath:'/QuizAppReact/',
+      },
+    },
+  },
   plugins: [react()],
 })
